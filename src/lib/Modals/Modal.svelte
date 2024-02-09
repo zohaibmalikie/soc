cb485f70486344a1eef598a30c440b7c47e163df
Ahmad Kamran <ahmad.kamran@selland.technology>
2024-02-10 04:12:20 +0500

    fixes

--- src/lib/Modals/Modal.svelte
+++ src/lib/Modals/Modal.svelte
@@ -194,7 +194,6 @@
 						type="button"
 						on:click={close}
 						class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-						autofocus
 					>
 						<svg
 							class="w-5 h-5"
